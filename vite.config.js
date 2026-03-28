ffc89b6af373046fcbb0643d85189e106f779f20
Rejoan Siddiky <siddikyrejoan@gmail.com>
2026-03-28 16:38:14 +0600

    Changed as Vecel Upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/git-repo-puller/",
+  base: process.env.VITE_BASE_PATH || "/git-repo-puller",
+  //  base: import.meta.env.VITE_BASE_PATH || "/git-repo-puller",
   server: {
     port: 3002,
     open: true,
