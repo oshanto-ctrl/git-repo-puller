65ff7a67a789e96ac6df6ed87444f6f4612c68c2
Rejoan Siddiky <siddikyrejoan@gmail.com>
2026-03-28 16:27:58 +0600

    Changed in package json for predeploy cmd add and vite for base /git-repo-puller/

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/git-repo-puller/",
   server: {
     port: 3002,
     open: true,
